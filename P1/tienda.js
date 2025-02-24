d6620dcaf077526ced29b4249a7d3f3ae04259bb
dperezg2021 <d.perezg.2021@alumnos.urjc.es>
2025-02-24 11:00:23 +0100

    sevrer

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,1 +1,43 @@
-hola
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8001;
+const PUBLIC_DIR = path.join(__dirname, 'public');
+
+const mimeTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml'
+};
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(PUBLIC_DIR, req.url === '/' ? 'index.html' : req.url);
+    let extname = path.extname(filePath);
+    
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                fs.readFile(path.join(PUBLIC_DIR, '404.html'), (err404, data404) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(err404 ? 'Página no encontrada' : data404);
+                });
+            } else {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error interno del servidor');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': mimeTypes[extname] || 'application/octet-stream' });
+            res.end(data);
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Servidor en ejecución en http://localhost:${PORT}`);
+});
