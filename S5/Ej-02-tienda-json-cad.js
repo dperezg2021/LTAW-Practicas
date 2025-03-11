be088a60adea0f404d68aea163870bad362a2cd8
dperezg2021 <d.perezg.2021@alumnos.urjc.es>
2025-03-11 13:21:36 +0100

    actu buscador

---
+++ S5/Ej-02-tienda-json-cad.js
@@ -0,0 +1,18 @@
+const cad = `[
+    {
+      "A": {
+        "a" : [50, 30]
+      },
+        "D": "Soy A"
+    },
+    {
+          "A": {
+        "b" : [33, 55]
+    },
+    "D": "Soy b"
+    }
+  ]`
+  
+
+  const f = JSON.parse(cad);
+  
---
+++ S5/prueba.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+fs.readFile('fich1.txt','utf8', (err, data) =>{
+    console.log("A");
+});
+console.log("B");
+const data1 = fs.readFileSync('fich2.txt','utf8',);
+console.log("C");
